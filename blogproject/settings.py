0e7c73b042761378829c02d8ed60d5287a761d10
Michael <michaelpaul3154@gmail.com>
2025-11-05 16:22:25 +0100

    'update'

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,17 +131,18 @@ LOGIN_REDIRECT_URL = 'Blog post'
 
 LOGIN_URL = 'users-login'
 
-STATIC_URL = 'static/'
+
 
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = (BASE_DIR / 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-] 
 
-STATIC_ROOT = (BASE_DIR / 'asset')
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "asset")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
